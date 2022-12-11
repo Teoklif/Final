b3415eafce5c243b3be8ba76146e72e6c42e4bd4
alpryakh <alpryakh@gmail.com>
2022-12-11 14:36:30 +0300

    game

---
+++ GetWarm/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+int temperature = 0;
+void GetWarm(int temper)
+{ 
+    temper = 0;
+    while (temper < 100)
+    {
+        Console.WriteLine($"temperature = {temper}. It's so cold yet");
+        Console.Write("Write 'getwarm' ");
+        string i = Console.ReadLine();
+        if (i.ToLower() == "getwarm")
+        {
+            temper += 25;
+        }
+
+
+    }
+}
+GetWarm(temperature);
+Console.WriteLine($"Thx, {name}. It's finally warm");
